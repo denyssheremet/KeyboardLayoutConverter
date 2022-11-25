8e06daf41572b4aa89546ac743a72e038ccde3de
Denys Sheremet <denys.sheremet@gmail.com>
2022-11-25 02:21:58 +0100

    Add more layouts and change to dict

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-var qwerty = '-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./_+QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?',
-    colemak= '-=qwfpgjluy;[]arstdhneio\'zxcvbkm,./_+QWFPGJLUY:{}ARSTDHNEIO"ZXCVBKM<>?',
-    dvorak = '[]\',.pyfgcrl/=aoeuidhtns-;qjkxbmwvz{}"<>PYFGCRL?+AOEUIDHTNS_:QJKXBMWVZ';
+const layouts = {
+    "qwerty":       '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?',
+    "dvorak":       '`1234567890[]\',.pyfgcrl/\\=aoeuidhtns-;qjkxbmwvz~!@#$%^&*(){}"<>PYFGCRL?+|AOEUIDHTNS_:QJKXBMWVZ',
+    "colemak":      '`1234567890-=qwfpgjluy;[]\\arstdhneio\'zxcvbkm,./~!@#$%^&*()_+QWFPGJLUY:{}|ARSTDHNEIO"ZXCVBKM<>?',
+    "colemak-dh":   '`1234567890-=qwfpbjluy;[]\\arstgkneio\'xcdvzmh,./~!@#$%^&*()_+QWFPBJLUY:{}|ARSTGKNEIO"XCDVZMH<>?',
+    "workman":      '`1234567890-=qdrwbjfup;[]\\ashtgyneoi\'zxmcvkl,./~!@#$%^&*()_+QDRWBJFUP:{}|ASHTGYNEOI"ZXMCVKL<>?',
+    "azerty FR":    '²&é"\'(-è_çà)=azertyuiop^$*qsdfghjklmùwxcvbn,;:! 1234567890°+AZERTYUIOP¨£µQSDFGHJKLM%WXCVBN?./§',
+    "azerty BEFR":  '²&é"\'(§è!çà)-azertyuiop^$µqsdfghjklmùwxcvbn,;:=³1234567890°_AZERTYUIOP¨*£QSDFGHJKLM%WXCVBN?./+',
+    "azerty BEDU":  '²&é"\'(§è!çà)-azertyuiop^$µqsdfghjklmùwxcvbn,;:=³1234567890°_AZERTYUIOP¨*£QSDFGHJKLM%WXCVBN?./+',
+}
+var from = layouts["qwerty"], to = layouts["qwerty"];
 
-var from = qwerty, to = qwerty;
-const layouts = [qwerty, colemak, dvorak];
+window.addEventListener('load', function() {
+    let selectFrom = this.document.getElementById("selectFrom");
+    let selectTo   = this.document.getElementById("selectTo");
 
+    for (const [key, value] of Object.entries(layouts)) {
+        selectFrom.add(new Option(key, value), undefined);
+        selectTo.add(new Option(key, value), undefined);
+      }
+});
 
 function convert() {
     let text = document.getElementById("input-field").value;
@@ -12,13 +26,11 @@ function convert() {
     for (var i = 0; i < text.length; i++) {
       converted += to.charAt(from.indexOf(text.charAt(i))) || text.charAt(i);
     }
-
     document.getElementById("output-field").innerHTML = converted;
 }
 
 function changeFromTo() {
-    from = layouts[document.getElementById("selectFrom").selectedIndex];
-    to = layouts[document.getElementById("selectTo").selectedIndex];
+    from = document.getElementById("selectFrom").value;
+    to   = document.getElementById("selectTo").value;
     convert();
 }
-
